59c89f458e4056eac1899bc305ccf16b17297192
drtran <DrKietTran@gmail.com>
2020-01-05 14:09:24 -0500

    initial checkin

---
+++ src/main/java/com/drkiettran/result_server/ResultServerApplication.java
@@ -0,0 +1,13 @@
+package com.drkiettran.result_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drkiettran/result_server/ResultServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drkiettran.result_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
